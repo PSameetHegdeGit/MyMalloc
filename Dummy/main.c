bae2a6e8d456c7656a18cd4a58aa185327f54a98
Sameet R. Hegde <srh155@butter.cs.rutgers.edu>
2019-10-12 23:48:16 -0400

    added ifs

---
+++ Dummy/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+    int n = 0;
+    if(n < 10){
+        printf("Winning!");
+        n++;
+
+        int j = 0;
+        if (j < 10){
+            printf("winning: 2");
+            j++;
+        }
+
+    }
+
+    return 0;
+
+
+}
